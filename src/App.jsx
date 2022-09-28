a414c8c1cf927bc3a84b4e7250ae840de0c00596
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-28 17:51:00 -0300

    se añade intervalos

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ function App() {
     }
   }, [index, people]);
 
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
     <section className="section">
       <div className="title">
